6a339446154c6e7ac51ba6a2001ae911d6da707b
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-05-12 14:40:21 +0530

    style: refactor dag

--- airflow/dags/first_dag.py
+++ airflow/dags/first_dag.py
@@ -12,10 +12,11 @@ def print_hello(ti, **context):
     
     return 'Hello world from first Airflow DAG!'
 
-dag = DAG('hello_world', description='Hello World DAG!',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+with DAG(
+    'hello_world', 
+    description='Hello World DAG!',
+    schedule_interval='0 12 * * *',
+    start_date=datetime(2017, 3, 20), catchup=False
+    ) as dag:
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
-
-hello_operator
+    task_1 = PythonOperator(task_id='hello_task', python_callable=print_hello)
