01be8db757c62917d616c5bcbe6393ed7d1d3803
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-05-12 12:27:49 +0530

    feat: log to file with IST

--- airflow/dags/first_dag.py
+++ airflow/dags/first_dag.py
@@ -1,9 +1,15 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+# from zoneinfo import ZoneInfo
 
-def print_hello():
+def print_hello(ti, **context):
+    
+    f = open("out.txt", "a")
+    f.write(f"{datetime.now() + timedelta(hours=5, minutes=30)} - Hello World!\n")
+    f.close()
+    
     return 'Hello world from first Airflow DAG!'
 
 dag = DAG('hello_world', description='Hello World DAG!',
