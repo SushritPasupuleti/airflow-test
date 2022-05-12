62fa56ea8caaf15f30200bce852204c6de4287d7
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-05-12 14:45:20 +0530

    feat: get multiple tasks working

--- airflow/dags/first_dag.py
+++ airflow/dags/first_dag.py
@@ -4,13 +4,25 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 # from zoneinfo import ZoneInfo
 
-def print_hello(ti, **context):
+def print_header(ti, **context):
     
     f = open("out.txt", "a")
-    f.write(f"{datetime.now() + timedelta(hours=5, minutes=30)} - Hello World! {context['dag_run'].conf['key']}\n")
+    f.write("================================\n")
+    f.write(f"Running DAG @{datetime.now() + timedelta(hours=5, minutes=30)}\n")
     f.close()
     
     return 'Hello world from first Airflow DAG!'
+
+def print_body(ti, **context):
+    
+    f=open("out.txt", "a")
+    f.write(f"Message: {context['dag_run'].conf['message']}\n")
+
+def print_footer(ti, **context):
+    f = open("out.txt", "a")
+    f.write(f"Cleaning up @{datetime.now() + timedelta(hours=5, minutes=30)}\n")
+    f.write("================================\n")
+    f.close()
 
 with DAG(
     'hello_world', 
@@ -19,4 +31,8 @@ with DAG(
     start_date=datetime(2017, 3, 20), catchup=False
     ) as dag:
 
-    task_1 = PythonOperator(task_id='hello_task', python_callable=print_hello)
+    task_1 = PythonOperator(task_id='header_task', python_callable=print_header)
+    task_2 = PythonOperator(task_id='body_task', python_callable=print_body)
+    task_3 = PythonOperator(task_id='footer_task', python_callable=print_footer)
+    
+    task_1 >> task_2 >> task_3
